e00c25365f9768fcdea261c756de9bdcab684929
Daniel Sayer <daniel.sayer01@gmail.com>
2024-04-27 13:14:18 +1000

    feat: create clients table

---
+++ src/lib/validations/clients.ts
@@ -0,0 +1,17 @@
+import * as z from "zod";
+
+export const registerClientSchema = z.object({
+  name: z
+    .string()
+    .min(1, "Name is required")
+    .max(255, "Name must be less that 256 characters"),
+  email: z.string().email().max(255, "Email must be less that 256 characters"),
+  phoneNumber: z
+    .string()
+    .regex(
+      new RegExp(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/),
+      "Must be a valid phone number",
+    )
+    .min(8, "Must be a valid phone number")
+    .max(15, "Must be a valid phone number"),
+});
