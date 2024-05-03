6c6331fd212459abf2ac528adb189e47600233a7
Daniel Sayer <daniel.sayer01@gmail.com>
2024-05-03 17:10:27 +1000

    init shad dashboard

---
+++ src/lib/greetings.ts
@@ -0,0 +1,12 @@
+export function getGreeting() {
+  const userHours = new Date().getHours();
+  if (userHours >= 17) {
+    return "Good evening";
+  }
+
+  if (userHours >= 12) {
+    return "Good afternoon";
+  }
+
+  return "Good morning";
+}
