862dbfa29756c77c4f56aed93351f308359c028b
Daniel Sayer <daniel.sayer01@gmail.com>
2024-05-06 18:12:19 +1000

    feat: init pricing page

--- src/app/settings/pricing/page.tsx
+++ src/app/settings/pricing/page.tsx
@@ -1,35 +1,85 @@
 import { Icons } from "@/components/Icons";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Separator } from "@/components/ui/separator";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency } from "@/lib/currencyUtils";
 
 export default function Page() {
   return (
     <div>
-      <Card className="mt-4">
-        <CardHeader>
-          <div className="flex items-center justify-between font-semibold">
-            Personal Information
-            <Button variant="outline" className="flex rounded-3xl">
-              <Icons.edit className="me-2 h-4 w-4" />
-              Edit
-            </Button>
-          </div>
-        </CardHeader>
-        <CardContent className="grid gap-2 md:grid-cols-2 lg:grid-cols-3">
-          <div>
-            <p className="text-sm text-muted-foreground">Name</p>
-            <p className="mt-2">Johnny</p>
-          </div>
-          <div>
-            <p className="text-sm text-muted-foreground">Email</p>
-            <p className="mt-2">johhny@email.ocm</p>
-          </div>
-          <div>
-            <p className="text-sm text-muted-foreground">Phone Number</p>
-            <p className="mt-2">911</p>
-          </div>
-        </CardContent>
-      </Card>
+      <div className="text-xl">Let&apos;s look into your pricing strategy!</div>
+      <RadioGroup defaultValue="set" className="mt-4 flex items-center gap-16">
+        <div className="flex items-center space-x-2">
+          <Icons.info className="h-5 w-5" />
+          <RadioGroupItem value="set" id="set" />
+          <Label htmlFor="set" className="text-xl">
+            Set
+          </Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Icons.info className="h-5 w-5" />
+          <RadioGroupItem value="value" id="value" />
+          <Label htmlFor="value" className="text-xl">
+            Value Based
+          </Label>
+        </div>
+      </RadioGroup>
+      <Separator className="my-4" />
+      <h3 className="flex items-center justify-between text-2xl font-medium">
+        Set Pricing Packages
+        <Button>
+          <Icons.add /> Pricing package
+        </Button>
+      </h3>
+      <p className="mt-2 text-muted-foreground">
+        The packages below are just examples, once you add one of your own these
+        will disappear
+      </p>
+
+      <Table className="mt-4">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Label</TableHead>
+            <TableHead>Price</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {defaultSetPrices.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.label}</TableCell>
+              <TableCell>{formatCurrency(item.price)}</TableCell>
+              <TableCell>Edit</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
+
+const defaultSetPrices = [
+  {
+    id: 1,
+    label: "30min Session",
+    price: 50,
+  },
+  {
+    id: 2,
+    label: "1hr Session",
+    price: 80,
+  },
+  {
+    id: 3,
+    label: "All day Session",
+    price: 300,
+  },
+];
