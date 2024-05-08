15f7de33d3198cebbfdf3564d1b71708e1b0eff5
Daniel Sayer <daniel.sayer01@gmail.com>
2024-05-08 20:39:58 +1000

    feat add empty state for upcoming events

--- src/components/dashboard/UpcomingEvents.tsx
+++ src/components/dashboard/UpcomingEvents.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { formatTimeRange } from "@/lib/dateRangeUtils";
-import { format } from "date-fns";
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { api } from "@/trpc/react";
+import type { TPRCReturnType } from "@/types/trpc";
+import { format } from "date-fns";
+import { Icons } from "../Icons";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Skeleton } from "../ui/skeleton";
 
 export const UpcomingEvents = () => {
@@ -14,36 +16,58 @@ export const UpcomingEvents = () => {
         <CardTitle>Upcoming Events</CardTitle>
       </CardHeader>
       <CardContent className="grid gap-2">
-        {isLoading || !data ? (
-          <EventSkeleton />
-        ) : (
-          data.map((event) => (
-            <div
-              key={event.id}
-              className="flex items-center gap-4 rounded border px-4 py-3"
-            >
-              <div className="grid gap-1">
-                <p className="text-md font-medium  leading-none">
-                  {event.name ? event.name : event.clients.name}
-                </p>
-                <p className="text-sm text-muted-foreground">
-                  {event.name ? event.clients.name : ""}
-                </p>
-              </div>
-              <div className="ml-auto grid gap-1">
-                <p className="text-sm font-medium leading-none">
-                  {formatTimeRange(event.startTime, event.endTime)}
-                </p>
-                <p className="text-sm text-muted-foreground">
-                  {format(event.startTime, "dd MMM yyyy")}
-                </p>
-              </div>
-            </div>
-          ))
-        )}
+        <UpcomingEventsContent data={data} isLoading={isLoading} />
       </CardContent>
     </Card>
   );
+};
+
+type UpcomingEventsContentProps = {
+  data: TPRCReturnType<typeof api.dashboard.getUpcomingEvents.useQuery>;
+  isLoading: boolean;
+};
+
+const UpcomingEventsContent = ({
+  data,
+  isLoading,
+}: UpcomingEventsContentProps) => {
+  if (isLoading || !data) {
+    return <EventSkeleton />;
+  }
+
+  if (data.length === 0) {
+    return (
+      <div className="grid place-items-center pt-6">
+        <div className="flex flex-col items-center gap-4">
+          <Icons.ghost className="h-6 w-6 text-muted-foreground" />
+          No upcoming events, create some!
+        </div>
+      </div>
+    );
+  }
+  return data.map((event) => (
+    <div
+      key={event.id}
+      className="flex items-center gap-4 rounded border px-4 py-3"
+    >
+      <div className="grid gap-1">
+        <p className="text-md font-medium  leading-none">
+          {event.name ? event.name : event.clients.name}
+        </p>
+        <p className="text-sm text-muted-foreground">
+          {event.name ? event.clients.name : ""}
+        </p>
+      </div>
+      <div className="ml-auto grid gap-1">
+        <p className="text-sm font-medium leading-none">
+          {formatTimeRange(event.startTime, event.endTime)}
+        </p>
+        <p className="text-sm text-muted-foreground">
+          {format(event.startTime, "dd MMM yyyy")}
+        </p>
+      </div>
+    </div>
+  ));
 };
 
 const EventSkeleton = () => {
@@ -64,3 +88,31 @@ const EventSkeleton = () => {
     </>
   );
 };
+
+// {isLoading || !data ? (
+//   <EventSkeleton />
+// ) : (
+//   data.map((event) => (
+//     <div
+//       key={event.id}
+//       className="flex items-center gap-4 rounded border px-4 py-3"
+//     >
+//       <div className="grid gap-1">
+//         <p className="text-md font-medium  leading-none">
+//           {event.name ? event.name : event.clients.name}
+//         </p>
+//         <p className="text-sm text-muted-foreground">
+//           {event.name ? event.clients.name : ""}
+//         </p>
+//       </div>
+//       <div className="ml-auto grid gap-1">
+//         <p className="text-sm font-medium leading-none">
+//           {formatTimeRange(event.startTime, event.endTime)}
+//         </p>
+//         <p className="text-sm text-muted-foreground">
+//           {format(event.startTime, "dd MMM yyyy")}
+//         </p>
+//       </div>
+//     </div>
+//   ))
+// )}
---
+++ src/types/trpc.ts
@@ -0,0 +1,6 @@
+import type { ProcedureUseQuery } from "node_modules/@trpc/react-query/dist/createTRPCReact";
+
+export type TPRCReturnType<T> = TRPCOutputType<T> | undefined;
+
+type TRPCOutputType<T> =
+  T extends ProcedureUseQuery<infer D> ? D["output"] : never;
